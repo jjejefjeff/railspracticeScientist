6d0643fb48bc17eec3cba82c9953d80d0cdd0288
Your Name <you@example.com>
2015-07-24 15:43:05 +0000

    add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/profile/:id' => 'application#show'
+  get '/create_scientist' => 'application#create'
+  get '/new_scientist/' => 'application#new_sci'
  # get '/profile/1' => 'application#first'
  # get '/profile/2' => 'application#second'
  # get '/profile/3' => 'application#third'
