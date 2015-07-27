8954b0269b22e5cc54e4a442fdad4578d96593ed
Your Name <you@example.com>
2015-07-27 15:21:30 +0000

    added updating forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,27 @@ class ApplicationController < ActionController::Base
   def show
     @scientist = Scientist.find_by_id(params['id'])
   end
+  
   def new_sci
   end
   
   def create
     s = Scientist.new
+    s.name = params['name']
+    s.imgurl = params['imgurl']
+    s.membersince = params['membersince']
+    s.description = params['description']
+    s.location = params['location']
+    s.completion = params['completion']
+    s.save
+    redirect_to "/profile/#{ s.id }"
+  end
+  def edit
+    @scientist = Scientist.find_by_id(params['id'])
+  end
+  
+  def update_scientist
+    s = Scientist.find_by_id(params['id'])
     s.name = params['name']
     s.imgurl = params['imgurl']
     s.membersince = params['membersince']
